8a8a95f2fa7bb464e2b53f7ed04653bf9d1beeae
seanglynn-thrive <sean.glynn@thriveglobal.com>
2021-11-15 21:50:55 +0000

    Config

---
+++ project/app/config.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", 0)
+
+# See LRU Cache: https://docs.python.org/3/library/functools.html#functools.lru_cache
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
