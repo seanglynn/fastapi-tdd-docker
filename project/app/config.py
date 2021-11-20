8e74140ce66d1e29ed6a22c5c05297fe2b248a00
seanglynn-thrive <seanglynn06@hotmail.com>
2021-11-20 12:22:50 +0000

    TortoiseORM

--- project/app/config.py
+++ project/app/config.py
@@ -11,6 +11,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool = os.getenv("TESTING", 0)
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
 # See LRU Cache: https://docs.python.org/3/library/functools.html#functools.lru_cache
 @lru_cache()
