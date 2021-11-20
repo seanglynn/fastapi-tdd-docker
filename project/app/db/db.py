27e1f066d659ffa2bfd9a2c348df8316f2d5b99f
seanglynn-thrive <seanglynn06@hotmail.com>
2021-11-20 12:22:16 +0000

    db integ with tortoiseORM

---
+++ project/app/db/db.py
@@ -0,0 +1,12 @@
+import os
+
+# DB ORM Definition - Required for migrations through Aerich
+TORTOISE_ORM = {
+    "connections": {"default": os.environ.get("DATABASE_URL")},
+    "apps": {
+        "models": {
+            "models": ["app.models.tortoise", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
