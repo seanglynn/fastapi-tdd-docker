e6364a563292e17fe803156eaf21ecb3f06a8eb0
seanglynn-thrive <seanglynn06@hotmail.com>
2021-11-23 08:18:17 +0000

    Get/Get_all routes

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -3,7 +3,8 @@ from fastapi import APIRouter, HTTPException
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
-
+from app.models.tortoise import SummarySchema
+from typing import List
 
 router = APIRouter()
 
@@ -17,3 +18,18 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
         "url": payload.url
     }
     return response_object
+
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    return summary
+
+
+@router.get("/", response_model=List[SummarySchema])
+async def read_all_summaries() -> List[SummarySchema]:
+    return await crud.get_all()
+
